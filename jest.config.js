3cb86a36eb0237c8e0b1306908f1e2b32df3cb46
Grey Elerson <grey.l.elerson@gmail.com>
2024-07-08 17:13:50 -0600

    chore: Update ts-jest dependency to version 29.2.0

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
+const { transform } = require("typescript");
+
 module.exports = {
   roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   coverageDirectory: './coverage/',
   verbose: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
