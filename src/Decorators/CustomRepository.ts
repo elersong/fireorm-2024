2061f75760c7b4640e1f853fff6fd16dcad3f822
Grey Elerson <grey.l.elerson@gmail.com>
2024-07-13 15:13:31 -0600

    feat(custom repository): register custom repository types to metadata via generics

--- src/Decorators/CustomRepository.ts
+++ src/Decorators/CustomRepository.ts
@@ -1,17 +1,26 @@
 import { getMetadataStorage } from '../MetadataUtils';
-import { Constructor, IRepository, IEntity } from '../types';
-import { BaseRepository } from '../BaseRepository';
+import { Constructor, IEntity } from '../types';
+import { BaseFirestoreRepository } from '../BaseFirestoreRepository';
+import { IEntityConstructor } from '../types';
 
-/*
-  Cannot enforce the type in target presumably becasuse Typescript
-  cannot verify than the T from the entity param is the same T from
-  the repository. Might be interesting to revisit later
-*/
+/**
+ * CustomRepository decorator to register a custom repository for a specific entity type.
+ *
+ * This decorator registers the given repository class as the custom repository
+ * for the specified entity type in the metadata storage. The custom repository
+ * class must extend the `BaseFirestoreRepository` class.
+ *
+ * @template T - The type of the entity.
+ * @param {Constructor<T>} entity - The constructor function of the entity type.
+ * @returns {Function} - A decorator function that takes the repository class and registers it.
+ */
 export function CustomRepository<T extends IEntity = IEntity>(entity: Constructor<T>) {
-  return function (target: BaseRepository) {
+  return function <U extends Constructor<BaseFirestoreRepository<T>>>(
+    target: U & { new (pathOrConstructor: string | IEntityConstructor): BaseFirestoreRepository<T> }
+  ): void {
     getMetadataStorage().setRepository({
       entity,
-      target: target as Constructor<IRepository<T>>,
+      target,
     });
   };
 }
