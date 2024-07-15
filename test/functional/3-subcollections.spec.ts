f9abf1a8bfa9b59901fe08cd3241283e013238f7
Grey Elerson <grey.l.elerson@gmail.com>
2024-07-15 15:53:53 -0600

    test: ensure subcollections use TransactionRepository inside transactions (#42)
    * test: add test to cover transaction subcollection case
    
    * style: prettier format
    
    * style: replace let with const
    
    * test: skip test with out-of-scope limitation and document for later work
    
    * style: prettier format
    
    * ci: ignore all test-related files from code coverage calculations

--- test/functional/3-subcollections.spec.ts
+++ test/functional/3-subcollections.spec.ts
@@ -1,6 +1,13 @@
 import { Band as BandEntity, Album as AlbumEntity, getInitialData } from '../fixture';
-import { getRepository, Collection, SubCollection, BaseFirestoreRepository } from '../../src';
-import { getUniqueColName } from '../setup';
+import {
+  getRepository,
+  Collection,
+  SubCollection,
+  BaseFirestoreRepository,
+  IEntity,
+} from '../../src';
+import { getUniqueColName, makeUnique } from '../setup';
+import { TransactionRepository } from '../../src/Transaction/BaseFirestoreTransactionRepository';
 
 describe('Integration test: SubCollections', () => {
   class Album extends AlbumEntity {}
@@ -22,7 +29,7 @@ describe('Integration test: SubCollections', () => {
 
     for (const s of seed) {
       const band = new FullBand();
-      band.id = s.band.id;
+      band.id = makeUnique(s.band.id);
       band.name = s.band.name;
       band.genres = s.band.genres;
       band.formationYear = s.band.formationYear;
@@ -33,7 +40,7 @@ describe('Integration test: SubCollections', () => {
 
       const albums = s.albums.map(a => {
         const album = new Album();
-        album.id = a.id;
+        album.id = makeUnique(a.id);
         album.releaseDate = a.releaseDate;
         album.name = a.name;
 
@@ -103,5 +110,80 @@ describe('Integration test: SubCollections', () => {
     await fullBandRepository.update(rush);
     const updatedRush = await fullBandRepository.findById('rush');
     expect(updatedRush.genres).toEqual(rush.genres);
+  });
+
+  it('should reinitialize subcollections as TransactionRepository within transactions', async () => {
+    const band = new FullBand();
+    band.id = 'kc-sunshine-band';
+    band.name = 'KC and the Sunshine Band';
+    band.formationYear = 1973;
+    band.genres = ['disco'];
+
+    await fullBandRepository.create(band);
+
+    const album = new Album();
+    album.id = 'do-it-good';
+    album.name = 'Do It Good';
+    album.releaseDate = new Date('1974-03-01');
+
+    await band.albums.create(album);
+
+    // Outside of a transaction, it should be a BaseFirestoreRepository
+    expect(band.albums).toBeInstanceOf(BaseFirestoreRepository);
+
+    // Within a transaction, the subcollection should be a TransactionRepository
+    fullBandRepository.runTransaction(async tran => {
+      const kc = await tran.findById('kc-sunshine-band');
+      expect(kc.albums).toBeInstanceOf(TransactionRepository);
+    });
+  });
+
+  // This test is limited by the inability to create two collections with the same
+  // entity constructor. This is because the entity constructor is used as the key
+  // in the collections map in MetadataStorage.
+  it.skip('should handle nested subcollections within transactions', async () => {
+    @Collection(getUniqueColName('label'))
+    class Label implements IEntity {
+      id: string;
+
+      @SubCollection(FullBand, makeUnique('band-subcollection'))
+      bands: BaseFirestoreRepository<FullBand>;
+    }
+
+    const labelRepository = getRepository(Label);
+    const label = new Label();
+    label.id = 'elektra-records';
+
+    await labelRepository.create(label);
+
+    await labelRepository.runTransaction(async tran => {
+      const elektra = await tran.findById('elektra-records');
+
+      const band = new FullBand();
+      band.id = 'the-doors';
+      band.name = 'The Doors';
+      band.formationYear = 1965;
+      band.genres = ['rock'];
+
+      await elektra.bands.create(band);
+
+      const album = new Album();
+      album.id = 'strange-days';
+      album.name = 'Strange Days';
+      album.releaseDate = new Date('1967-09-25');
+
+      const theDoors = await elektra.bands.findById('the-doors');
+      await theDoors.albums.create(album);
+
+      const fetchedAlbums = await theDoors.albums.find();
+      expect(fetchedAlbums.length).toEqual(1);
+      expect(fetchedAlbums[0].name).toEqual('Strange Days');
+    });
+
+    const finalElektra = await labelRepository.findById('elektra-records');
+    const finalDoors = await finalElektra.bands.findById('the-doors');
+    const finalAlbums = await finalDoors.albums.find();
+    expect(finalAlbums.length).toEqual(1);
+    expect(finalAlbums[0].name).toEqual('Strange Days');
   });
 });
