58e81eafebb1a552ea2a39d05bd3614813bd11da
Grey Elerson <grey.l.elerson@gmail.com>
2024-07-21 00:46:55 -0600

    refactor(errors): extend NoMetadataError

--- src/Errors/index.ts
+++ src/Errors/index.ts
@@ -1,11 +1,21 @@
 import type { EntityConstructorOrPath, IEntity } from '../types';
 
 export class NoMetadataError extends Error {
-  constructor(pathOrConstructor: EntityConstructorOrPath<IEntity>) {
+  constructor(pathOrConstructorOrCollectionName: EntityConstructorOrPath<IEntity>) {
+    let name;
+    if (typeof pathOrConstructorOrCollectionName === 'string') {
+      if (pathOrConstructorOrCollectionName.includes("/")) {
+        // String is a path to a subcollection
+        name = `subcollection named: ${pathOrConstructorOrCollectionName}`
+      } else {
+        // String is a top level collection name
+        name = `collection named: ${pathOrConstructorOrCollectionName}`
+      }
+    } else {
+      name = `constructor named: ${pathOrConstructorOrCollectionName.name}` 
+    }
     super(
-      `There is no metadata stored for "${
-        typeof pathOrConstructor === 'string' ? pathOrConstructor : pathOrConstructor.name
-      }"`
+      `There is no metadata stored for "${ name }"`
     );
   }
 }
