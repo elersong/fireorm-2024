d211672189890141b79fd62e1610ad5067d11328
Grey Elerson <grey.l.elerson@gmail.com>
2024-07-25 15:06:06 -0600

    refactor(test): remove deep nesting

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -4,42 +4,40 @@ import { extractAllGetters, serializeEntity } from './utils';
 
 describe('Utils', () => {
   describe('extractAllGetter', () => {
-    describe('Class', () => {
-      it('should return only getters not data property', () => {
-        class ClassTest {
-          public a = 'a';
-          private _c;
+    it('should return only getters, not data properties of ClassTest', () => {
+      class ClassTest {
+        public a = 'a';
+        private _c;
 
-          public get b() {
-            return 'b';
-          }
+        public get b() {
+          return 'b';
         }
+      }
 
-        const b = new ClassTest();
+      const b = new ClassTest();
 
-        const extracted = extractAllGetters(b as unknown as Record<string, unknown>);
-        expect(extracted).toEqual({ b: 'b' });
-      });
-
-      it('should return only getters not method', () => {
-        class ClassTest {
-          public get b() {
-            return 'b';
-          }
-
-          public a() {
-            return 'a method';
-          }
-        }
-
-        const b = new ClassTest();
-
-        const extracted = extractAllGetters(b as unknown as Record<string, unknown>);
-        expect(extracted).toEqual({ b: 'b' });
-      });
+      const extracted = extractAllGetters(b as unknown as Record<string, unknown>);
+      expect(extracted).toEqual({ b: 'b' });
     });
 
-    it('should return only getters which return undefined', () => {
+    it('should return only getters, not methods of ClassTest', () => {
+      class ClassTest {
+        public get b() {
+          return 'b';
+        }
+
+        public a() {
+          return 'a method';
+        }
+      }
+
+      const b = new ClassTest();
+
+      const extracted = extractAllGetters(b as unknown as Record<string, unknown>);
+      expect(extracted).toEqual({ b: 'b' });
+    });
+
+    it('should return only getters which do not return undefined', () => {
       class ClassTest {
         public a = 'a';
 
